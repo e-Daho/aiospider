6ca0f109f467e77b63f134ef6ae53e28688406d4
Thomas Perrot <thomas.perrot@cybelangel.com>
2016-09-18 22:42:38 +0200

    Added link extraction

--- tor_spider.py
+++ tor_spider.py
@@ -1,13 +1,24 @@
-import urllib.parse
-import time
+import logging
+import re
+from typing import Iterable
 
-import socks
+from tld import get_tld
+from bs4 import BeautifulSoup
 import asyncio
 from typing import Dict
 import aiohttp
+import async_timeout
 import aiosocks
-from http_parser.parser import HttpParser
 from aiosocks.connector import SocksConnector
+from concurrent.futures._base import TimeoutError
+import motor.motor_asyncio
+
+# TODO: one queue for internal links, one queue for external links, redis cache
+
+crawled_urls = set()
+client = motor.motor_asyncio.AsyncIOMotorClient()
+db = client['test']
+coll = db['crawls']
 
 
 class SiteNotFound(Exception):
@@ -38,115 +49,108 @@ class Response:
     def has_status_code(self):
         return bool(self.status_code)
 
+    def has_content(self):
+        return bool(self.content)
 
-async def get2(url: str):
 
-    conn = SocksConnector(proxy=aiosocks.Socks5Addr('127.0.0.1', 9150), proxy_auth=None, remote_resolve=True)
+def get_internal_links(bs_obj: BeautifulSoup, include_url: str) -> Iterable[str]:
+    """Finds all links that begin with a "/" or which contain the page url"""
+
+    already_saw = {include_url}
+    for link in bs_obj.find_all("a", href=re.compile(r"^(/(?!/)|.*" + get_tld('http://' + include_url) + ")")):
+        if link.attrs['href'] is not None:
+            ref = link.attrs['href']
+            if ref.startswith('http'):
+                url = ref.split('//')[1]
+            elif ref.startswith('/'):
+                url = include_url + ref
+            else:
+                logging.info('The internal link {} does not start with "http" or "/"'.format(ref))
+                continue
+            url = url.strip('/')
+            if url not in already_saw:
+                already_saw.add(url)
+                yield url
+
+
+def get_external_links(bs_obj: BeautifulSoup, exclude_url: str) -> Iterable[str]:
+    """Finds all links that start with "http" or "www" that do not contain the current URL"""
+
+    already_saw = {exclude_url}
+    exclude_domain = exclude_url.replace('http://', '')
+    for link in bs_obj.find_all("a", href=re.compile(r"^(http|https|www)(?!"+exclude_domain+").*$")):
+        if link.attrs['href'] is not None:
+            link = link.attrs['href']
+            if link not in already_saw and link.endswith('.onion'):
+                already_saw.add(link)
+                yield link
+
+
+async def get(session: aiohttp.ClientSession, url: str):
+
+    headers = {
+        'Connection': 'keep-alive',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/39.0.2171.95 Safari/537.36',
+        'Referrer': 'https:/www.google.com/',
+        'Accept-Language': 'en-US,en;q=0.8',
+        'Accept-Encoding': 'gzip, deflate, sdch',
+    }
+
     try:
-        with aiohttp.ClientSession(connector=conn) as session:
-            async with session.get(url) as resp:
-                r = Response(url)
-                r.status_code = resp.status
-                r.headers = resp.headers
-                r.content = await resp.text()
-                return r
-    except aiohttp.ProxyConnectionError:
-        # connection problem
-        print('oups')
-    except aiosocks.SocksError:
-        # communication problem
-        print('oula')
+            with async_timeout.timeout(20):
+                async with session.get(url, headers=headers) as resp:
+                    r = Response(url, status_code=resp.status, headers=resp.headers)
+                    r.content = await resp.read()
+                    return r
+    except TimeoutError:
+        return None
+    except aiohttp.ProxyConnectionError as err:
+        logging.exception(err)
+    except aiosocks.SocksError as err:
+        logging.exception(err)
 
 
-async def get(url: str, allow_redirects: bool=True, referrer: str='https://www.google.com/') -> Response:
-    """Performs a GET request on the given url. Returns a Response object."""
+async def crawl(queue):
+    """Get a new url from the Queue, crawls it, store page in Mongodb, extracts external links, adds them to Queue"""
 
-    url_obj = urllib.parse.urlsplit(url)
-    s = socks.socksocket()
-
-    # Sends request
-    # if url_obj.scheme == 'https':
-    #     s.connect((url_obj.hostname, 443))
-    #     connect = asyncio.open_connection(server_hostname='', ssl=True, sock=s)
-    # else:
-    #     s.connect((url_obj.hostname, 80))
-    #     connect = asyncio.open_connection(sock=s)
-    # reader, writer = await connect
-
-    reader, writer = await aiosocks.open_connection(
-        proxy=aiosocks.Socks5Addr('127.0.0.1', 9150),
-        # dst=('url', 443),
-        dst=('url', 80),
-        remote_resolve=True,
-        proxy_auth=None
-    )
-
-    query = ('GET {path} HTTP/1.0\r\n'
-             'Host: {hostname}\r\n'
-             'Connection: keep-alive\r\n'
-             'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\r\n'
-             'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) '
-             'Chrome/39.0.2171.95 Safari/537.36\r\n'
-             'Referrer: {referrer}\r\n'
-             # 'Accept-Encoding: gzip, deflate, sdch\r\n'
-             'Accept-Language: en-US,en;q=0.8\r\n'
-             '\r\n').format(path=url_obj.path or '/', hostname=url_obj.hostname, referrer=referrer)
-    writer.write(query.encode('latin-1'))
-
-    # Reads response
-    p = HttpParser()
-    body = []
-    r = Response(url)
-    while True:
-        line = await reader.readline()
-        if not line:
-            break
-        recved = len(line)
-        nparsed = p.execute(line, recved)
-        assert nparsed == recved
-        if not r.has_status_code():
-            status_code = int(line.decode('utf8').split(' ')[1])
-            r.status_code = status_code
-        if p.is_headers_complete() and not r.has_headers():
-            r.headers = dict(p.get_headers())
-            # Immediately follow redirection if allowed, don't wait for the body
-            if r.status_code in (301, 302) and allow_redirects:
-                writer.close()
-                return await get(r.headers['Location'], allow_redirects=allow_redirects, referrer=url)
-        if p.is_partial_body():
-            body.append(p.recv_body())
-        if p.is_message_complete():
-            break
-    r.content = b''.join(body)
-
-    writer.close()
-    if r.status_code == 0:
-        raise SiteNotFound
-
-    return r
-
-
-async def test(url):
-
-    print(url)
-    r = await get2(url)
-    print(r.status_code)
-    # print(r.headers)
+    bulk = []
+    conn = SocksConnector(proxy=aiosocks.Socks5Addr('127.0.0.1', 9150), proxy_auth=None, remote_resolve=True)
+    with aiohttp.ClientSession(connector=conn) as session:
+        while not queue.empty():
+            url = await queue.get()
+            print(url)
+            crawled_urls.add(url)
+            r = await get(session, url)
+            if r and r.has_content:
+                bulk.append({'_id': url, 'source': r.content})
+                if len(bulk) >= 1:
+                    await coll.insert(bulk)
+                    count = await db.test.count()
+                    logging.debug('Inserted {} new documents in database'.format(count))
+                    bulk = []
+                bs_obj = BeautifulSoup(r.content, 'html.parser')
+                for new_url in get_external_links(bs_obj, url):
+                    if new_url not in crawled_urls:
+                        queue.put_nowait(new_url)
 
 
 def main():
 
-    # socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 9150, True)
     loop = asyncio.get_event_loop()
-    # task = asyncio.ensure_future(test('http://oxwugzccvk3dk6tj.onion/'))
-    tasks = [
-        asyncio.ensure_future(test('http://www.github.com')),
-        asyncio.ensure_future(test('http://oxwugzccvk3dk6tj.onion/')),
-        asyncio.ensure_future(test('http://vichandcxw4gm3wy.onion/')),
-        asyncio.ensure_future(test('http://gurochanocizhuhg.onion/')),
-        asyncio.ensure_future(test('http://xiwayy2kn32bo3ko.onion/')),
-        asyncio.ensure_future(test('http://redditor3a2spgd6.onion'))
+    queue = asyncio.Queue()
+    urls = [
+        'http://www.github.com',
+        'http://oxwugzccvk3dk6tj.onion/',
+        'http://vichandcxw4gm3wy.onion/',
+        'http://gurochanocizhuhg.onion/',
+        'http://xiwayy2kn32bo3ko.onion/',
+        'http://redditor3a2spgd6.onion'
     ]
+    for url in urls:
+        queue.put_nowait(url)
+    tasks = [asyncio.ensure_future(crawl(queue)) for _ in range(100)]
 
     loop.run_until_complete(asyncio.gather(*tasks))
 
